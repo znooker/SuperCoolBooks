8af089c56223b44cfc490a2d6dbc7448a7e6d1d8
sala3132156 <116171488+sala3132156@users.noreply.github.com>
2023-04-13 11:41:47 +0200

    added genre model

---
+++ SuperCoolBooks/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperCoolBooks.Models
+{
+    public class Genre
+    {
+        public int GenreID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
+
