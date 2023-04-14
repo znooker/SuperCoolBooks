a51d6e046e62162bd873ccb1e1ff5d47c8e61f64
znooker <znooker_18@hotmail.com>
2023-04-14 13:55:24 +0200

    Create Genre
    Not fully done, missing validation on serverside

--- SuperCoolBooks/Models/Genre.cs
+++ SuperCoolBooks/Models/Genre.cs
@@ -2,12 +2,12 @@
 {
     public class Genre
     {
-        public int GenreID { get; set; }
+        public int GenreId { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public DateTime Created { get; set; }
 
-        public ICollection<Book> Books { get; set; }
+        public ICollection<Book>? Books { get; set; }
     }
 }
 
