e48e46c1397dd30fdbb8cf82913cbb086b01768e
Sheldozer <125883340+Sheldozer@users.noreply.github.com>
2023-04-21 17:51:58 +0200

    Can now give feedback to a review. (upvote/downvote)

---
+++ SuperCoolBooks/Data/Migrations/20230421142124_AddReviewFeedback.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperCoolBooks.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewFeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReviewFeedBacks",
+                columns: table => new
+                {
+                    ReviewFeedBackId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsHelpful = table.Column<bool>(type: "bit", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReviewFeedBacks", x => x.ReviewFeedBackId);
+                    table.ForeignKey(
+                        name: "FK_ReviewFeedBacks_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReviewFeedBacks_Reviews_ReviewId",
+                        column: x => x.ReviewId,
+                        principalTable: "Reviews",
+                        principalColumn: "ReviewId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReviewFeedBacks_ReviewId",
+                table: "ReviewFeedBacks",
+                column: "ReviewId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReviewFeedBacks_UserId",
+                table: "ReviewFeedBacks",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReviewFeedBacks");
+        }
+    }
+}
---
+++ SuperCoolBooks/Models/ReviewFeedback.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuperCoolBooks.Models
+{
+    public class ReviewFeedback
+    {
+        public int ReviewFeedBackId { get; set; }
+        public bool? IsHelpful { get; set; }
+
+        //Foreign keys
+        public string UserId { get; set; }
+        public int ReviewId { get; set; }
+
+        //Navigation props
+        public virtual AspNetUser? User { get; set; }
+        public virtual Review? Review { get; set; }
+    }
+}
